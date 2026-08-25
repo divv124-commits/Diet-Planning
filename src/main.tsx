3ed3251343cba617a790dddfbfd1c1f886299a34
Bhupendra Yadav <bhupendrayadav@Bhupendras-MacBook-Air.local>
2026-08-25 20:30:47 +0530

    Adding GA4

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-3982H5MSV0')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
